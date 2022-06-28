a269075ba556ce7488d2c8f54c4aebf80be9b5b9
Oleh Davymuka <o.davymuka@sharksw.com>
2022-06-28 13:11:29 +0300

    Test GitLab & GitHub sync

--- myproject/boards/tests.py
+++ myproject/boards/tests.py
@@ -10,6 +10,11 @@ class HomeTests(TestCase):
         response = self.client.get(url)
         self.assertEquals(response.status_code, 200)
 
+    def test_home_view_status_code_2(self):
+        url = reverse("home")
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
     def test_home_url_resolves_home_view(self):
         view = resolve("/")
         self.assertEquals(view.func, home)
